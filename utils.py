56c8dbc051d0713a18c2031744268e58c285dbe1
MarkKai <742714625@qq.com>
2019-10-29 12:56:13 -0400

    func memory_reduce

--- utils.py
+++ utils.py
@@ -58,22 +58,24 @@ def reduce_mem_usage(df, logger=None, verbose=True):
     for col in df.columns:
         col_type = df[col].dtypes
         if col_type in numerics:
-            _diff = df[col].max() - df[col].min()
+            c_min = df[col].min()
+            c_max = df[col].max()
             if str(col_type)[:3] == 'int':
-                if _diff <= (np.iinfo(np.int8).max - np.iinfo(np.int8).min):
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
                     df[col] = df[col].astype(np.int8)
-                elif _diff <= (np.iinfo(np.int16).max - np.iinfo(np.int16).min):
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
                     df[col] = df[col].astype(np.int16)
-                elif _diff <= (np.iinfo(np.int32).max - np.iinfo(np.int32).min):
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
                     df[col] = df[col].astype(np.int32)
-                elif _diff <= (np.iinfo(np.int64).max - np.iinfo(np.int64).min):
-                    df[col] = df[col].astype(np.int64)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
             else:
-                if _diff <= (np.iinfo(np.float16).max - np.iinfo(np.float16).min):
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
                     df[col] = df[col].astype(np.float16)
-                elif _diff <= (np.iinfo(np.float32).max - np.iinfo(np.float32).min):
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                     df[col] = df[col].astype(np.float32)
-                else: df[col] = df[col].astype(np.float64)
+                else:
+                    df[col] = df[col].astype(np.float64)
     end_mem = df.memory_usage().sum() / 1024**2
     if verbose and logger:
         logger.info(f'Mem. usage decreased to {end_mem:5.2f} Mb ({(start_mem - end_mem)/start_mem:.1f}% reduction)')
