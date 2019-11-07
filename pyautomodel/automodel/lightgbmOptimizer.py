3862c2684f3e357d0a723248394fe9c2fac49fc2
MarkKai <742714625@qq.com>
2019-11-07 18:43:19 -0500

    lightgbm update

--- pyautomodel/automodel/lightgbmOptimizer.py
+++ pyautomodel/automodel/lightgbmOptimizer.py
@@ -1,11 +1,11 @@
 #!usr/bin python3
-
 import lightgbm
 import json
 import pickle
 import numpy as np
 import pandas as pd
 from preprocessor import Preprocessor
+from sklearn.model_selection import train_test_split
 from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
 
 
@@ -14,20 +14,17 @@ class LightgbmOptimizerBinary:
     Use hyperopt to optimize lightgbm binary classifier.
     """
 
-    def __init__(self, X_train, y_train, X_eval, y_eval, X_test, y_test,
+    def __init__(self, data, label,
                  base_params: dict, cat_params: dict, int_params: dict, float_params: dict,
                  num_opts=1000, trials_path='./trials.pkl', load_trials=False,
                  lgb_num_boost_round=3000, lgb_early_stopping_rounds=400, 
-                 cv=None, strategy='stratified', group=None, shuffle=True, random_state=1213):
+                 test_size=0.25, shuffle=True, random_state=1213,
+                 cv=None, strategy='stratified', group=None):
         """LightGBM hyperparameters optimizer initializer.
         
         Arguments:
-            X_train {DataFrame} -- training dataset
-            y_train {Series} -- training labels
-            X_eval {DataFrame} -- evaluation dataset
-            y_eval {Series} -- evaluation labels
-            X_test {DataFrame} -- testing dataset
-            y_test {Series} -- testing labels
+            data {DataFrame} -- dataset for training
+            label {Series} -- labels for training
             cat_params {dict} -- categorical hyperparameters to tune
             int_params {dict} -- integer hyperparameters to tune
             float_params {dict} -- float/double hyperparameters to tune
@@ -39,53 +36,52 @@ class LightgbmOptimizerBinary:
             lgb_num_boost_round {int} -- number of training rounds for lightgbm model (default: {3000})
             lgb_early_stopping_rounds {int} -- early stopping rounds for lightgbm model (default: {400})
         """
-        # data
-        self.X_train, self.X_eval, self.X_test = X_train, X_eval, X_test
-        self.y_train, self.y_eval, self.y_test = y_train, y_eval, y_test
-        self.train_dataset = lightgbm.Dataset(data=self.X_train, label=self.y_train)
-        self.eval_dataset = lightgbm.Dataset(data=self.X_eval, label=self.y_eval)
-        self.test_dataset = lightgbm.Dataset(data=self.X_test, label=self.y_test)
-        # hyperparameters
-        self.base_params, self.cat_params = base_params, cat_params
-        self.int_params, self.float_params = int_params, float_params
-        self.all_params = self._init_params()
-        self.best_params = None
-        # lightgbm other hyperparameter
-        self.lgb_num_boost_round, self.lgb_early_stopping_rounds = lgb_num_boost_round, lgb_early_stopping_rounds
-        # optimizer
-        self.num_opts, self.trials_path, self.load_trials = num_opts, trials_path, load_trials
-        self.trials = self._init_trials()
-        # whether use CV to trian and optimize model
+        # store data and label we need to use this to generate folds generator
+        self.data = data
+        self.label = label
         if cv:
+            # parameters for making folds dataset
             self._n_splits = cv
             self._strategy = strategy
             self._group = group
-            self._shuffle = shuffle
-            self._random_state = random_state
+            # construct dataset
+            self.dataset = lightgbm.Dataset(self.data, self.label)
+        # parameters for making hold out dataset
+        self._test_size = test_size
+        self._shuffle = shuffle
+        self._random_state = random_state
+        # construct training and evaluating dataset
+        self.train_dataset, self.eval_dataset = self._make_hold_out(data, label)
+        # hyperparameters
+        self._base_params, self._cat_params = base_params, cat_params
+        self._int_params, self._float_params = int_params, float_params
+        self._all_params = self._init_params()
+        # lightgbm other hyperparameter
+        self._lgb_num_boost_round, self._lgb_early_stopping_rounds = lgb_num_boost_round, lgb_early_stopping_rounds
+        # optimizer
+        self._num_opts, self._trials_path, self._load_trials = num_opts, trials_path, load_trials
+        self.trials = self._init_trials()
 
     def _init_params(self):
         """
         Initialize hyperparameters.
         """
         # categorical hyperparameters
-        self.cat_params_hp = {param: hp.choice(param, candidates)
-                              for param, candidates in self.cat_params.items()}
+        cat_params_hp = {param: hp.choice(param, candidates) for param, candidates in self._cat_params.items()}
         # integer hyperparameters
-        self.int_params_hp = {param: hp.choice(param, np.arange(*start_end_step, dtype=np.int))
-                              for param, start_end_step in self.int_params.items()}
+        int_params_hp = {param: hp.choice(param, np.arange(*start_end_step, dtype=np.int))
+                              for param, start_end_step in self._int_params.items()}
         # float hyperparameters
-        self.float_params_hp = {param: hp.uniform(param, *candidates)
-                                for param, candidates in self.float_params.items()}
+        float_params_hp = {param: hp.uniform(param, *candidates) for param, candidates in self._float_params.items()}
         # generate all hyperparameters
-        return dict(self.base_params, **self.cat_params_hp,
-                    **self.int_params_hp, **self.float_params_hp)
+        return dict(self._base_params, **cat_params_hp, **int_params_hp, **float_params_hp)
 
     def _init_trials(self):
         """
         Initialize trials database.
         """
-        if self.load_trials:
-            trials = pickle.load(open(self.trials_path, "rb"))
+        if self._load_trials:
+            trials = pickle.load(open(self._trials_path, "rb"))
             current_iter = len(trials.losses())
             self.num_opts += current_iter
         else:
@@ -93,7 +89,7 @@ class LightgbmOptimizerBinary:
         return trials
 
     def _init_folds(self):
-        self._folds = Preprocessor.make_folds(self.X_train, self.y_train, 
+        self._folds = Preprocessor.make_folds(self.data, self.label, 
                                               n_splits=self._n_splits, strategy=self._strategy, group=self._group,
                                               shuffle=self._shuffle, random_state=self._random_state)
 
@@ -101,16 +97,28 @@ class LightgbmOptimizerBinary:
         """
         Convert hyperopt.choice optimized index back to original values
         """
-        if not hasattr(self, 'best_params'):
-            raise ValueError('Best hyperparameters not exist.')
-        if not self.best_params:
-            raise AttributeError('Model has not been trained.')
-        # convert categorical from index back to value
-        for key, value in self.cat_params.items():
-            self.best_params[key] = value[self.best_params[key]]
-        # convert integer from index back to value
-        for key, value in self.int_params.items():
-            self.best_params[key] = np.arange(*value, dtype=np.int)[self.best_params[key]]
+        if hasattr(self, 'best_params'):
+            # convert categorical from index back to value
+            for key, value in self._cat_params.items():
+                self.best_params[key] = value[self.best_params[key]]
+            # convert integer from index back to value
+            for key, value in self._int_params.items():
+                self.best_params[key] = np.arange(*value, dtype=np.int)[self.best_params[key]]
+        else:
+            raise AttributeError('Best hyperparameters not exist.')
+
+    def _make_hold_out(self, data, label):
+        """
+        Split training and evaluating set
+        Return (train_set, eval_set)
+        """
+        X_train, X_eval, y_train, y_eval = train_test_split(data, label, 
+                                                            test_size=self._test_size, 
+                                                            random_state=self._random_state, 
+                                                            shuffle=self._shuffle)
+        train_set = lightgbm.Dataset(X_train, y_train)
+        eval_set = lightgbm.Dataset(X_eval, y_eval)
+        return train_set, eval_set
 
     def _object_score(self, params):
         """
@@ -118,23 +126,23 @@ class LightgbmOptimizerBinary:
         """
         lgb_clf = lightgbm.train(params=params,
                                  train_set=self.train_dataset,
-                                 num_boost_round=self.lgb_num_boost_round,
+                                 num_boost_round=self._lgb_num_boost_round,
                                  valid_sets=[self.train_dataset, self.eval_dataset],
                                  valid_names=['Train', 'Eval'],
-                                 early_stopping_rounds=self.lgb_early_stopping_rounds,
+                                 early_stopping_rounds=self._lgb_early_stopping_rounds,
                                  verbose_eval=-1,
                                  callbacks=[lightgbm.callback.reset_parameter(
                                      learning_rate=lambda current_round: 0.6 * (0.99 ** current_round))])
         # define return dict
         ret_dict = {'status': STATUS_OK}
         # get auc results
-        auc_train = best_score['Train']['auc']
-        auc_eval = best_score['Eval']['auc']
+        auc_train = lgb_clf.best_score['Train']['auc']
+        auc_eval = lgb_clf.best_score['Eval']['auc']
         ret_dict.update({'train_auc': auc_train, 'eval_auc': auc_eval})
         # get all other metrics results
         if 'binary_error' in params['metric'].split(','):
-            error_train = best_score['Train']['binary_error']
-            error_eval = best_score['Eval']['binary_error']
+            error_train = lgb_clf.best_score['Train']['binary_error']
+            error_eval = lgb_clf.best_score['Eval']['binary_error']
             ret_dict.update({'train_error': error_train, 'eval_error': error_eval})
         # define loss
         # we invoke difference between train auc and eval auc as penalty
@@ -148,23 +156,18 @@ class LightgbmOptimizerBinary:
         """
         Using all hyperparameters to train LightGBM in a CV fashion. Return the objective function score.
         """
-        # we need to initialize folds every time
+        # we need to initialize folds every time training LightGBM because it's a generator
         self._init_folds()
+        # get evaluation results
         eval_hist = lightgbm.cv(params=params,
-                                train_set=self.train_dataset,
-                                num_boost_round=self.lgb_num_boost_round,
+                                train_set=self.dataset,
+                                num_boost_round=self._lgb_num_boost_round,
                                 folds=self._folds,
                                 nfold=None, stratified=None, shuffle=None, seed=None,
-                                metrics=None,
-                                fobj=None,
-                                feval=None,
-                                init_model=None,
-                                feature_name='auto',
-                                categorical_feature='auto',
-                                early_stopping_rounds=self.lgb_early_stopping_rounds,
-                                fpreproc=None,
-                                verbose_eval=-1,
-                                show_stdv=True,
+                                metrics=None, fobj=None, feval=None, init_model=None,
+                                feature_name='auto', categorical_feature='auto',
+                                early_stopping_rounds=self._lgb_early_stopping_rounds,
+                                fpreproc=None, verbose_eval=-1, show_stdv=True,
                                 callbacks=[lightgbm.callback.reset_parameter(
                                      learning_rate=lambda current_round: 0.6 * (0.99 ** current_round))],
                                 eval_train_metric=False)
@@ -187,15 +190,13 @@ class LightgbmOptimizerBinary:
         The main entrance of optimizing LightGBM model.
         """
         if hasattr(self, '_n_splits'):
-            best_params = fmin(self._object_score_cv, self.all_params, algo=tpe.suggest,
-                               max_evals=self.num_opts, trials=self.trials)
+            self.best_params = fmin(self._object_score_cv, self._all_params, algo=tpe.suggest,
+                               max_evals=self._num_opts, trials=self.trials)
         else:
-            best_params = fmin(self._object_score, self.all_params, algo=tpe.suggest,
-                               max_evals=self.num_opts, trials=self.trials)
+            self.best_params = fmin(self._object_score, self._all_params, algo=tpe.suggest,
+                               max_evals=self._num_opts, trials=self.trials)
         # save trials for further fine-tune
-        pickle.dump(self.trials, open(self.trials_path, "wb"))
-        # store best hyperparameters
-        self.best_params = best_params
+        pickle.dump(self.trials, open(self._trials_path, "wb"))
         # there are some params in best_params is index value
         # we need to convert back to actual value
         self._index2value()
@@ -204,18 +205,23 @@ class LightgbmOptimizerBinary:
         """
         Use best hyperparameters to train lihgtgbm model.
         """
-        lgb_clf = lightgbm.train(params=dict(self.best_params, **self.base_params),
+        # check if the hyperparameters is tuned
+        if not hasattr(self, 'best_params'):
+            raise AttributeError('Best hyperparameters not exist.')
+        lgb_clf = lightgbm.train(params=dict(self.best_params, **self._base_params),
                                  train_set=self.train_dataset,
-                                 num_boost_round=self.lgb_num_boost_round,
+                                 num_boost_round=self._lgb_num_boost_round,
                                  valid_sets=[self.train_dataset, self.eval_dataset],
                                  valid_names=['Train', 'Eval'],
-                                 early_stopping_rounds=self.lgb_early_stopping_rounds,
+                                 early_stopping_rounds=self._lgb_early_stopping_rounds,
                                  verbose_eval=1,
                                  callbacks=[lightgbm.callback.reset_parameter(
                                      learning_rate=lambda current_round: 0.6 * (0.99 ** current_round))])
         return lgb_clf
 
     def get_best_params(self):
-        if not self.best_params:
+        if hasattr(self, 'best_params'):
+            return self.best_params
+        else:
             raise AttributeError('Best hyperparameters not exist.')
-        return self.best_params
+
